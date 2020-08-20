61be61e6c40ffd3ef1206ad1b890ff0471ab3d60
kazuNittc <31737@toyota.kosen-ac.jp>
2020-08-20 10:59:03 +0900

    Update server.js
    IDが重複したらデータを更新する処理を追加

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ class MyServer extends Server {
     api(path, req) {
         if (path === "/api/setalarm") {
             var json = JSON.parse(Deno.readTextFileSync('./alarm.json'));
-            json.push(req);
+            // 重複を確認 なければ追加 あれば更新
+            const dup = json.find(dat => dat.id === req.id);
+            if (dup === undefined) {
+                json.push(req);
+            } else {
+                json.find(dat => {
+                    if (dat.id === req.id) {
+                        dat.time = req.time;
+                    }
+                });
+            }
             Deno.writeTextFileSync("./alarm.json", JSON.stringify(json));
         } else if (path === "/api/getalarm") {
             const json = JSON.parse(Deno.readTextFileSync('./alarm.json'));
