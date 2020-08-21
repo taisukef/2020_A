96aef9f7786645d236fdcecbc095f639595b84de
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2020-08-21 13:28:58 +0900

    Add: ミッションの答えを送信する機能追加

---
+++ static/util.js
@@ -0,0 +1,11 @@
+export function getUuid() {
+    var arr = new Array();
+    if (document.cookie != '') {
+        var tmp = document.cookie.split('; ');
+        for (var i = 0; i < tmp.length; i++) {
+            var data = tmp[i].split('=');
+            arr[data[0]] = decodeURIComponent(data[1]);
+        }
+    }
+    return arr['uuid'];
+}
