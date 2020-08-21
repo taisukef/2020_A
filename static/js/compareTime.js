03a843e700b6ca7182e441d6f4b7bf2b634b039a
harukamatoba <45505097+harukamatoba@users.noreply.github.com>
2020-08-21 09:00:32 +0900

    Update compareTime.js

--- static/js/compareTime.js
+++ static/js/compareTime.js
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html><body>
-    Test
-    <div id="test1">
-
-    </div>
-    <!-- <script src="../js/compareTime.js"></script> -->
-    <script type="module">
-
-        import { compareTime } from "./js/compareTime.js";
-        const testdate = new Date(2020,7,20,11,42).getTime();
-        // 仮の設定時間の定義
-        window.onload =  function () {
-            var cnt = setInterval(function(){
-                var cmp = compareTime(testdate);
-                if(cmp >= 0) {
-                    clearInterval(cnt);
-                    // cmp(compareTimeの返り値)が0以上なら時間が過ぎているのでループを抜ける
-                }
-
-            },100);
-            // 100msごとにまわしている
-
-        }
-    </script>
-</body></html>
+// 過ぎたら1 丁度なら0 まだなら-1
+// UTCからの時間経過が引数
+function compareTime(setTime){
+    var now = new Date();
+    var nowTime = now.getTime();
+    console.log(setTime,nowTime);
+    if(setTime < nowTime) {
+        return 1;
+    } else if(setTime > nowTime ){
+        return -1;
+    } else {
+        return 0;
+    }
+}
+export {compareTime}
